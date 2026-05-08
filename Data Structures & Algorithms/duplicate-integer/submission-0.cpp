9371533621163bf2b81842a39e58263c8662a8a0
Dandeheden <dandeheden433@gmail.com>
2026-05-08 21:49:14 +0530

    Add: duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        set<int>s;
+        for(int i : nums){
+            s.insert(i);
+        }
+        if(s.size()==nums.size()){
+            return false;
+        }
+        return true;
+    }
+};
